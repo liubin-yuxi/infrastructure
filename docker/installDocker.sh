0e8c794ccc1d29dfc6398be911c2355dc581476b
liubin-yuxi <liubin@yuxisoft.cn>
2018-11-09 16:51:14 +0800

    aa

---
+++ docker/installDocker.sh
@@ -0,0 +1,22 @@
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install docker-ce
+
+#sudo groupadd docker     #添加docker用户组
+sudo gpasswd -a jenkins docker     #将登陆用户加入到docker用户组中
+newgrp docker     #更新用户组
+docker ps  
