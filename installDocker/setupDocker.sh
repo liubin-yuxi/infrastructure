bf5ce93354ccfa714ca02f7e5207d8ae698c1841
liubin-yuxi <liubin@yuxisoft.cn>
2018-12-01 15:44:54 +0800

    ff

---
+++ installDocker/setupDocker.sh
@@ -0,0 +1,10 @@
+mkdir -p /etc/systemd/system/docker.service.d
+
+path=/etc/systemd/system/docker.service.d/http-proxy.conf
+
+echo ' [Service] ' >> $path
+echo ' ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:4567 -H unix:///var/run/docker.sock ' >> $path
+
+systemctl daemon-reload
+systemctl restart docker
+ps -ef | grep docker
