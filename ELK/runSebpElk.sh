159097e680eb6079ba20da75d40fa6ea8264449d
liubin-yuxi <liubin@yuxisoft.cn>
2018-12-09 23:17:50 +0800

    ff

--- ELK/runSebpElk.sh
+++ ELK/runSebpElk.sh
@@ -1,4 +1,4 @@
-docker run --name elk  -p 5601:5601 -p 9200:9200  -p 5044:5044 \
+docker run -d --name elk  -p 5601:5601 -p 9200:9200  -p 5044:5044 \
 -v elk-data:/var/lib/elasticsearch \
 -v /etc/logstash:/etc/logstash \
 -v /etc/elasticsearch/elasticsearch.yml:/etc/elasticsearch/elasticsearch.yml \
