57a2e6968c870a95fed81181209ca366687002ac
liubin-yuxi <liubin@yuxisoft.cn>
2019-11-09 16:33:14 +0800

    dd

--- ELK/javahome.sh
+++ ELK/javahome.sh
@@ -1,3 +1,7 @@
+mkdir -p /usr/lib/java
+touch ~/.bashrc
+tar -zxf jdk-8u192-linux-x64.tar.gz -C /usr/lib/java
+
 echo 'export JAVA_HOME=/usr/lib/java/jdk1.8.0_192' >> ~/.bashrc   
 echo 'export JRE_HOME=${JAVA_HOME}/jre  ' >> ~/.bashrc   
 echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib  ' >> ~/.bashrc   
