686ea8745f18f5560145fb2ebfcc5f935a5d8bb7
liubin-yuxi <liubin@yuxisoft.cn>
2019-11-09 16:34:17 +0800

    ff

--- installDocker/installDocker.sh
+++ installDocker/installDocker.sh
@@ -17,7 +17,7 @@ apt-get update
 apt-get install -y docker-ce
 
 #sudo groupadd docker     #添加docker用户组
-sudo gpasswd -a jenkins docker     #将登陆用户加入到docker用户组中
+#sudo gpasswd -a jenkins docker     #将登陆用户加入到docker用户组中
 newgrp docker     #更新用户组
 docker ps  
 
