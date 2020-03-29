f6747359c4e40f4f2cbd6c2ab339992fff78d2d3
liubin-yuxi <liubin@yuxisoft.cn>
2020-03-29 15:13:50 +0800

    ff

--- ELK/javahome.sh
+++ ELK/javahome.sh
@@ -1,11 +1,11 @@
-mkdir -p /usr/lib/java
-touch ~/.bashrc
-tar -zxf jdk-8u192-linux-x64.tar.gz -C /usr/lib/java
+sudo mkdir -p /usr/lib/java
+sudo touch ~/.bashrc
+sudo tar -zxf jdk-8u192-linux-x64.tar.gz -C /usr/lib/java
 
-echo 'export JAVA_HOME=/usr/lib/java/jdk1.8.0_192' >> ~/.bashrc   
-echo 'export JRE_HOME=${JAVA_HOME}/jre  ' >> ~/.bashrc   
-echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib  ' >> ~/.bashrc   
-echo 'export PATH=${JAVA_HOME}/bin:$PATH  ' >> ~/.bashrc   
+sudo echo 'export JAVA_HOME=/usr/lib/java/jdk1.8.0_192' >> ~/.bashrc   
+sudo echo 'export JRE_HOME=${JAVA_HOME}/jre  ' >> ~/.bashrc   
+sudo echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib  ' >> ~/.bashrc   
+sudo echo 'export PATH=${JAVA_HOME}/bin:$PATH  ' >> ~/.bashrc   
 
 source ~/.bashrc
 
